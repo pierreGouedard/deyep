d18d9e78cb1a3848ee7552488d0d0d89f4d41737
pierre <pierre@realytics.io>
2018-05-09 13:59:19 +0200

    Init deyep project 10

--- deyep/core/tools/linear_algebra.py
+++ deyep/core/tools/linear_algebra.py
@@ -1,5 +1,33 @@
 # Global import
 import numpy as np
+from scipy.sparse import lil_matrix
+from pathos.multiprocessing import ProcessingPool as Pool, cpu_count
+
+
+class FourrierSeriesParallel(object):
+
+    def __init__(self, coef):
+        self.coef = coef
+
+    def f(self, x):
+        return self.coef * np.exp(-1j * x)
+
+
+class InnerProductParallel(object):
+    def __init__(self, keep_real=False, round=False):
+        self.keep_real = keep_real
+        self.round = round
+
+    def f(self, t):
+        res = inner_product(get_fourrier_series(t[0]), get_fourrier_series(t[1]))
+
+        if self.keep_real:
+            res = np.real(res)
+
+        if self.round:
+            res = np.round(res)
+
+        return res
 
 
 def get_fourrier_coef(N, k):
@@ -22,15 +50,217 @@ def get_fourrier_key(coef):
     return N, k
 
 
-def get_fourrier_series(coef):
+def get_fourrier_series(coef, n_jobs=1):
 
     # Get N
     N, k = get_fourrier_key(coef)
 
-    return np.array([coef * np.exp(np.complex(0, - (2. * np.pi * k * t) / N)) for t in range(-1, N - 1)])
+    if n_jobs != 1:
+        # Build Pool
+        p = Pool({0: cpu_count()}.get(n_jobs, n_jobs))
+
+        # Instantiate class that generate Fourrier series
+        fourrier = FourrierSeriesParallel(coef)
+
+        # Parallel Fourrier series building
+        res = np.array(p.map(fourrier.f, (2. * np.pi * k * np.arange(-1, N - 1)) / N))
+
+    else:
+        res = coef * np.exp(-1j * (2. * np.pi * k * np.arange(-1, N - 1)) / N)
+
+    return res
 
 
 def inner_product(x, y):
+    """
+    x and y are 1d numpy array of the same size
+
+    :param x:
+    :param y:
+    :return:
+    """
     return x.dot(y.conjugate())
 
 
+def vector_product_type_1(x, y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    try:
+        return x.toarray()[0].dot(y.toarray()[0])
+    except MemoryError:
+        return x.dot(y.tocsc().transpose())[0, 0]
+
+
+def vector_product_type_2(x, y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    return x.tocsc().transpose().dot(y)
+
+
+def vector_product_type_3(x, A):
+    """
+
+    :param x:
+    :param A:
+    :return:
+    """
+    try:
+        return lil_matrix(x.toarray()[0].dot(A.toarray()))
+    except MemoryError:
+        return x.dot(A)
+
+
+def matrix_product(A, B):
+    """
+
+    :param A:
+    :param B:
+    :return:
+    """
+    n, d = float(A.nnz * np.prod(B.shape) + B.nnz * np.prod(A.shape)), np.prod(A.shape) * np.prod(B.shape)
+    sparsity = 1 - (n / d)
+
+    if sparsity >= 0.6:
+        return A.dot(B)
+    else:
+        try:
+            return lil_matrix(A.toarray().dot(B.toarray()))
+        except MemoryError:
+            return A.dot(B)
+
+
+def matrix_fourrier_product(A, B, n_jobs=1):
+    """
+    To PARALLELIZE
+    :param A:
+    :param B:
+    :param method:
+    :return:
+    """
+
+    # Build Pool if necessary
+    if n_jobs != 1:
+        p = Pool({0: cpu_count()}.get(n_jobs, n_jobs))
+
+    (l_xa, l_ya), (l_xb, l_yb), res = A.nonzero(), B.nonzero(), lil_matrix((A.shape[0], B.shape[1]))
+    for i in set(l_xa):
+        for j in set(l_yb):
+            l_indices = list(set(l_ya[l_xa == i]).intersection(set(l_xb[l_yb == j])))
+
+            # multiprocess version
+            if n_jobs != 1:
+                # Instantiate class that implement inner product
+                innerp = InnerProductParallel(keep_real=True, round=True)
+
+                # Parallel inner product
+                res_ = sum(p.map(innerp.f, zip(A[i, l_indices].toarray()[0], B[l_indices, j].toarray()[:, 0])))
+
+            # single process version
+            else:
+
+                res_ = 0
+                for k in l_indices:
+                    res_ += np.round(np.real(inner_product(get_fourrier_series(A[i, k]), get_fourrier_series(B[k, j]))))
+
+            if res_ > 0:
+                res[i, j] = res_
+
+    return res
+
+
+def vector_fourrier_diag(x, y, n_jobs=1):
+    """
+    To PARALLELIZE
+
+    :param x:
+    :param y:
+    :param method:
+    :return:
+    """
+
+    res, l_indices = lil_matrix(x.shape), list(set(x.nonzero()[1]).intersection(y.nonzero()[1]))
+
+    if n_jobs != 1:
+
+        p = Pool({0: cpu_count()}.get(n_jobs, n_jobs))
+
+        # Instantiate class that implement inner product
+        innerp = InnerProductParallel(keep_real=True, round=True)
+
+        # Parallel inner product
+        res_ = lil_matrix(p.map(innerp.f, zip(x[0, l_indices].toarray()[0], y[0, l_indices].toarray()[0])))
+
+    else:
+        # Loop on common non zeros diag indices
+        res_ = []
+        for i in l_indices:
+            res_ += [inner_product(get_fourrier_series(x[0, i]), get_fourrier_series(y[0, i]))]
+
+        res_ = lil_matrix(res_)
+
+    # Set result sparse matrix
+    res[0, l_indices] = res_
+
+    return res
+
+
+def Chi(x):
+    """
+
+    :param x:
+    :return:
+    """
+    if x > 0:
+        return 1
+    else:
+        return 0
+
+
+def Chi_fourrier(x, l_coefs, n_jobs=1, freq_map=None):
+
+    res = 0
+
+    # set freq map to identity in case None
+    if freq_map is None:
+        freq_map = dict(l_coefs, l_coefs)
+
+    for c in l_coefs:
+        res += Chi(np.round(np.real(inner_product(get_fourrier_series(x), get_fourrier_series(c))))) * freq_map[c]
+
+    return res
+
+
+def Upsilon(x):
+    """
+
+    :param x:
+    :return:
+    """
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
+
+
+def Upsilon_fourrier(x):
+
+    res = 0
+
+    # set freq map to identity in case None
+    if freq_map is None:
+        freq_map = dict(l_coefs, l_coefs)
+
+    for c in l_coefs:
+        res += Chi(np.round(np.real(inner_product(get_fourrier_series(x), get_fourrier_series(c))))) * freq_map[c]
+
+    return res
--- tests/core/operators.py
+++ tests/core/operators.py
@@ -1,12 +1,14 @@
 # Global imports
 import numpy as np
 import unittest
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix, csr_matrix, csc_matrix
 import time
 
 # Local import
 from deyep.core.constructors.constructors import Constructor
-from deyep.core.tools.linear_algebra import inner_product, get_fourrier_coef, get_fourrier_series
+from deyep.core.tools.linear_algebra import inner_product, get_fourrier_coef, get_fourrier_key, get_fourrier_series, \
+    vector_product_type_1, vector_product_type_2, vector_product_type_3, matrix_product, matrix_fourrier_product, \
+    vector_fourrier_diag, Chi_fourrier,  Upsilon_fourrier
 
 __maintainer__ = 'Pierre Gouedard'
 
@@ -14,17 +16,23 @@ __maintainer__ = 'Pierre Gouedard'
 class TestOperators(unittest.TestCase):
     def setUp(self):
 
+        np.random.seed(303)
         # Create a simple deep network (2 input nodes, 3 network nodes,, 2 output nodes)
-        self.N = 100
+        self.N = 1000000
+        self.n = int(np.sqrt(self.N))
+        self.sparsity = 0.8
 
         self.vect_fourrier = np.array([get_fourrier_coef(self.N, k) for k in range(self.N)])
-        self.mat_fourrier = self.vect_fourrier.reshape((10, 10))
+        self.vect_fourrier[np.random.choice(range(self.N), int(self.N * self.sparsity), replace=False)] = 0.
+        self.mat_fourrier = self.vect_fourrier.reshape((self.n, self.n))
 
-        self.vector_bool = np.random.randint(0, 2, 100).astype(bool)
-        self.mat_bool = self.vector_bool.reshape(10, 10)
+        self.vector_bool = np.random.randint(0, 2, self.N).astype(bool)
+        self.vector_bool[np.random.choice(range(self.N), int(self.N * self.sparsity), replace=False)] = 0.
+        self.mat_bool = self.vector_bool.reshape(self.n, self.n)
 
-        self.vector_real = np.random.randint(0, 20, 100)
-        self.mat_real = self.vector_real.reshape(10, 10)
+        self.vector_real = np.random.randint(0, 20, self.N)
+        self.vector_real[np.random.choice(range(self.N), int(self.N * self.sparsity), replace=False)] = 0.
+        self.mat_real = self.vector_real.reshape(self.n, self.n)
 
     def test_operators_fourrier(self):
         """
@@ -32,41 +40,98 @@ class TestOperators(unittest.TestCase):
         python -m unittest tests.core.operators.TestOperators.test_operators_fourrier
 
         """
+        # i, j = np.random.randint(0, self.N), np.random.randint(0, self.N)
+        # x, y = self.vect_fourrier[i], self.vect_fourrier[j]
+        #
+        # # Test Multiplication
+        # res = (1. / np.linalg.norm(x)) * x * y
+        # self.assertAlmostEqual(np.real(self.vect_fourrier[(i + j) % self.N] - res), 0, delta=1e-10)
+        # self.assertAlmostEqual(np.imag(self.vect_fourrier[(i + j) % self.N] - res), 0, delta=1e-10)
+        #
+        # # Test inner product
+        # t0 = time.time()
+        # res_x = np.real(inner_product(get_fourrier_series(x), get_fourrier_series(x)))
+        # res_y = np.real(inner_product(get_fourrier_series(y), get_fourrier_series(y)))
+        # res_ = np.real(inner_product(get_fourrier_series(y), get_fourrier_series(x)))
+        # _res = np.real(inner_product(get_fourrier_series(x), get_fourrier_series(y)))
+        # print('time for inner product in R is {} seconds'.format((time.time() - t0) / 4.))
+        #
+        # self.assertAlmostEqual(res_x, 1.0, delta=1e-9)
+        # self.assertAlmostEqual(res_y, 1.0, delta=1e-9)
+        # self.assertAlmostEqual(res_, 0.0, delta=1e-9)
+        # self.assertAlmostEqual(_res, 0.0, delta=1e-9)
+        # self.assertAlmostEqual(res_, _res, delta=1e-9)
 
-        i, j = np.random.randint(0, 100), np.random.randint(0, 100)
-        x, y = self.vect_fourrier[i], self.vect_fourrier[j]
-        # Test Multiplication
-        res = (1. / np.linalg.norm(x)) * x * y
-        self.assertAlmostEqual(np.real(self.vect_fourrier[(i + j) % 100] - res), 0, delta=1e-10)
-        self.assertAlmostEqual(np.imag(self.vect_fourrier[(i + j) % 100] - res), 0, delta=1e-10)
-
-        # Test inner product
-        res_x = np.real(inner_product(get_fourrier_series(x), get_fourrier_series(x)))
-        res_y = np.real(inner_product(get_fourrier_series(y), get_fourrier_series(y)))
-        res_ = np.real(inner_product(get_fourrier_series(y), get_fourrier_series(x)))
-        _res = np.real(inner_product(get_fourrier_series(x), get_fourrier_series(y)))
-
-        self.assertAlmostEqual(res_x, 1.0, delta=1e-9)
-        self.assertAlmostEqual(res_y, 1.0, delta=1e-9)
-        self.assertAlmostEqual(res_, 0.0, delta=1e-9)
-        self.assertAlmostEqual(_res, 0.0, delta=1e-9)
-        self.assertAlmostEqual(res_, _res, delta=1e-9)
-
-        # Test running time
-        x_, y_ = get_fourrier_coef(500000, i), get_fourrier_coef(500000, j)
-
+        # Type 1 operation; vector - vector = vector (1 core) SPARSE
         t0 = time.time()
-        _ = np.real(inner_product(get_fourrier_series(x_), get_fourrier_series(y_)))
-        print('time for inner product in F_500000 is {} seconds'.format(time.time() - t0))
+        for i in range(10):
+            res = vector_product_type_1(lil_matrix(self.vect_fourrier), lil_matrix(self.vect_fourrier))
+        print('Mean time for vector product in F_{} is {} seconds'.format(self.N, (time.time() - t0) / 10.))
 
+        # # Type 2 vector - vector => matrix (1 cores) SPARSE
+        # vect_x, vect_y = lil_matrix(self.vect_fourrier[:self.n * 10]), lil_matrix(self.vect_fourrier[:(self.n * 10)])
+        # t0 = time.time()
+        #
+        # for i in range(10):
+        #     res = vector_product_type_2(vect_x, vect_y)
+        #
+        # print('Mean time for vector type 2 product in F_{} is {} seconds'.format(self.N, (time.time() - t0) / 10.))
+        #
+        # self.assertEqual(res.shape,  (vect_x.shape[1], vect_y.shape[1]))
+        # self.assertEqual(get_fourrier_key(np.sqrt(self.N) * res[289, 472]), (self.N, 761))
+        #
+        # # Type 3 vector matrix => vector (1 core) SPARSE
+        # vect_x, mat_x = lil_matrix(self.vect_fourrier[:self.n]), lil_matrix(self.mat_fourrier)
+        # t0 = time.time()
+        # for i in range(10):
+        #     res = vector_product_type_3(vect_x, mat_x)
+        #
+        # print('Mean time for vector - matrix product in F_{} is {} seconds'.format(self.N, (time.time() - t0) / 10.))
+        # self.assertEqual(res.shape, (1, mat_x.shape[-1]))
+        #
+        # # Type 4 matrix - matrix => matrix (all core) NO SPARSE
+        # t0 = time.time()
+        # mat_x, mat_y = lil_matrix(self.mat_fourrier), lil_matrix(self.mat_fourrier)
+        # for i in range(10):
+        #     res = matrix_product(mat_x, mat_y)
+        #
+        # print('Mean time for matrix - matrix product in F_{} is {} seconds'.format(self.N, (time.time() - t0) / 10.))
+        # self.assertEqual(res.shape, mat_x.shape)
+        #
+        # # Test matrix_fourrier_product
+        # mat_x, mat_y = lil_matrix(self.mat_fourrier[:7, :7]), lil_matrix(self.mat_fourrier[:7, :7])
+        # t0 = time.time()
+        # res = matrix_fourrier_product(mat_x, mat_y)
+        # print('Mean time for matrix - matrix fourrier product in F_{} is {} seconds'
+        #       .format(self.N, (time.time() - t0)))
+        #
+        # self.assertEqual(res.nnz, len(mat_x.diagonal().nonzero()[0]))
+        #
+        # # Test matrix_fourrier_product MULTIPROCESS
+        # t0 = time.time()
+        # res = matrix_fourrier_product(mat_x, mat_y, n_jobs=0)
+        # print('MULTIPROCESS Mean time for matrix - matrix fourrier product in F_{} is {} seconds'
+        #       .format(self.N, (time.time() - t0)))
+        #
+        # self.assertEqual(res.nnz, len(mat_x.diagonal().nonzero()[0]))
+        #
+        # # Test vector_fourrier_type_2
+        # vect_x, vect_y = lil_matrix(self.vect_fourrier), lil_matrix(self.vect_fourrier)
+        #
+        # t0 = time.time()
+        # res = vector_fourrier_diag(vect_x[0, :1000], vect_y[0, :1000], n_jobs=0)
+        # print('MULTIPROCESS get series in F_{} is {} seconds'.format(self.N, (time.time() - t0)))
+        #
+        # self.assertTrue((res.nonzero()[1] == vect_x[0, :1000].nonzero()[1]).all())
         import IPython
         IPython.embed()
-        # Test matrix multiplication in F_N
-
-
-        # Test matrix multiplication in from F_N to R
 
         # Test Chi in F_N
+        coefs, d_coefs = self.vect_fourrier[:10], dict(zip(self.vect_fourrier[:10], [1] * 10))
+        x = np.random.randint(10)
+
+        x, y = Chi_fourrier(coefs.dot(x), coefs, freq_map=d_coefs)
+
 
         # Test Upsilon in F_N
 
@@ -105,5 +170,3 @@ class TestOperators(unittest.TestCase):
         # Test Chi in R
 
         # Test Upsilon in R
-
-
